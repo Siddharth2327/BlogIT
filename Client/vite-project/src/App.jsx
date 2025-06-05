ecb520644f6d8bcb4534b515a8af68f853a79355
Siddharth <msiddharth298@gmail.com>
2025-06-05 22:45:59 +0530

    First push

---
+++ Client/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-yellow-300 underline text-3xl font-bold'> Hello from the react</h1>
+    </>
+  )
+}
+
+export default App
